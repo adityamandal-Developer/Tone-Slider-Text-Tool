334c1d25e87b863d6d66c83e2e006b8e27c2fa84
adityamandal-Developer <mandaladitya901@gmail.com>
2025-05-01 18:01:44 +0530

    added themes

--- src/components/ui/change-theme-button.tsx
+++ src/components/ui/change-theme-button.tsx
@@ -7,19 +7,10 @@ import { cn } from "@/lib/utils";
 import { Button } from "../ui/button";
 import { Command, CommandGroup, CommandItem, CommandList } from "../ui/command";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
-
-const themes = [
-  {
-    value: "mono",
-    label: "mono",
-  },
-  {
-    value: "dark",
-    label: "dark",
-  },
-];
+import { useTheme } from "next-themes";
 
 const ChangeThemeButton = () => {
+  const { setTheme, themes, theme } = useTheme();
   const [open, setOpen] = React.useState(false);
   const [value, setValue] = React.useState("");
 
@@ -32,9 +23,7 @@ const ChangeThemeButton = () => {
           aria-expanded={open}
           className="w-[200px] justify-between"
         >
-          {value
-            ? themes.find((theme) => theme.value === value)?.label
-            : "Select theme"}
+          {theme}
           <ChevronsUpDown className="opacity-50" />
         </Button>
       </PopoverTrigger>
@@ -44,18 +33,20 @@ const ChangeThemeButton = () => {
             <CommandGroup>
               {themes.map((theme) => (
                 <CommandItem
-                  key={theme.value}
-                  value={theme.value}
+                  key={theme}
+                  value={theme}
                   onSelect={(currentValue) => {
                     setValue(currentValue === value ? "" : currentValue);
                     setOpen(false);
+                    setTheme(theme);
+                    console.log(theme);
                   }}
                 >
-                  {theme.label}
+                  {theme}
                   <Check
                     className={cn(
                       "ml-auto",
-                      value === theme.value ? "opacity-100" : "opacity-0"
+                      value === theme ? "opacity-100" : "opacity-0"
                     )}
                   />
                 </CommandItem>
